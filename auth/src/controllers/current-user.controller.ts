74e9800066f763f03d49c5296598c01abbd84b88
Mario Alabd <marioalabd611@gmail.com>
2026-08-11 01:42:43 +0300

    refactor: remove unused imports from current user controller

--- auth/src/controllers/current-user.controller.ts
+++ auth/src/controllers/current-user.controller.ts
@@ -1,9 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 
-import { UnauthorizedError } from "../errors/index..errors";
 import { success } from "../utils/responses";
-
-import JWT from "../services/JWT";
 
 const currentUser = async (req: Request, res: Response, next: NextFunction) => {
     success(res, req.currentUser, "Current user retrieved successfully");
